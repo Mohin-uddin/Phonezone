3e443bc821ac7bca3eee2573e212076d6ee9ed0f
mohiuddin-san <mohi@san-developer.com>
2026-06-03 02:22:53 +0900

    Update API URL for production

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://phonezone-backend-huow.onrender.com/api' });
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('pz_token');
